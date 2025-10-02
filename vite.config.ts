e8beb1c2c84f19d2e2a204b41fd307d96f822663
nikki-ad <nikki.adhami.99@gmail.com>
2025-10-02 18:22:48 +0330

    second

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,16 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0", // تغییر به 0.0.0.0
     port: 8080,
+    allowedHosts: [
+      "6422ecc8-645b-4b77-89aa-9835be3883a4-00-30k7f02hsowzj.worf.replit.dev" // اضافه کردن هاست مجاز
+    ],
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
